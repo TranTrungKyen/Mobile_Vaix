3b9201d56f663bc4a8ad087339f0f2067c6449c7
KienTT <trantrungkien280502@gmail.com>
2024-10-02 11:49:45 +0700

    feat create migration

---
+++ database/migrations/2024_10_02_041555_create_product_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id'); // foreign key
+            $table->integer('color_id'); // foreign key
+            $table->integer('storage_id'); // foreign key
+            $table->integer('quantity');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
